d225d15877b9889ba9b2ce479bdc5fcce0103ea4
Mateus Vilione <mateusvilione@gmail.com>
2020-08-05 17:32:49 -0300

    connection Create

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import express, { request, response } from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
+routes.post('/classes', classesController.create)
+routes.get('/classes', classesController.index)
 
-routes.post('/classes', classesController.create);
+routes.post('/connections', connectionsController.create)
 
 export default routes;
